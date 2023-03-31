4ad29a7cb5fbb0a4b612379533dd3e8fd5bc58e1
sunwooKim <frod5@kakao.com>
2023-03-31 13:15:56 +0900

    스프링 mvc 1편 - spring mvc 시작 - method = RequestMethod.GET,POST를 @GetMapping, @PostMapping 어노테이션으로 변환

--- src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -4,9 +4,7 @@ import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,19 +14,19 @@ public class SpringMemberControllerV3 {
 
     private final MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    @GetMapping("/new-form")
     public String newForm() {
         return "new-form";
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public String members(Model model) {
         List<Member> result = memberRepository.findAll();
         model.addAttribute("members", result);
         return "members";
     }
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping("/save")
     public String save(
             @RequestParam("username") String username,
             @RequestParam("age") int age,
